cffd96210fa80ce3238aad6680cd4fd9a1949993
dangquanghuy1 <huyzzznnn23@gmail.com>
2023-07-26 20:10:55 +0700

    first commit

--- server/index.js
+++ server/index.js
@@ -2,33 +2,25 @@ const express = require('express');
 const app = express();
 const http = require("http");
 const cors = require('cors');
-const {Server} = require("socket.io")
-app.use(cors());
+const { Server } = require("socket.io");
+const axios = require('axios'); // Thêm thư viện Axios
+
+app.use(cors({
+  origin: "verdant-fairy-4ac903.netlify.app", // Thay đổi thành tên miền của frontend đã triển khai
+  methods: ["GET", "POST"],
+}));
 
 const server = http.createServer(app);
 
-const io = new Server(server,{
-    cors:{
-        origin:"http://localhost:3000",
-        methods:["GET","POST"],        
-    }
-})
-
-io.on("connection",(socket)=>{
-   console.log(`User Connect: ${socket.id}`);
-  
-    socket.on("join_room",(data)=>{
-        socket.join(data)
-        console.log(`User with ID: ${socket.id} joined room:${data}`)
-    })
-     socket.on("send_message",(data)=>{
-      socket.to(data.room).emit("receive_message",data)
-     })
-    socket.on("disconnect",()=>{
-        console.log("User Disconnected",socket.id);
-    })
+const io = new Server(server, {
+  cors: {
+    origin: "verdant-fairy-4ac903.netlify.app", // Thay đổi thành tên miền của frontend đã triển khai
+    methods: ["GET", "POST"],
+  }
 });
 
-server.listen(3001,()=>{
-    console.log("server run");
-})
+// ... Tiếp tục phần còn lại của mã backend giống như trước đó ...
+
+server.listen(3001, () => {
+  console.log("server run");
+});
